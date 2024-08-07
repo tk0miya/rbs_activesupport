4514f1d93ffc4c493bccc2e6aed0699641b429ca
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-08-07 21:23:47 +0900

    Initial commit (just after "bundle gem rbs_activesupport_delegate")

---
+++ lib/rbs_activesupport_delegate.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rbs_activesupport_delegate/version"
+
+module RbsActivesupportDelegate
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rbs_activesupport_delegate/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RbsActivesupportDelegate
+  VERSION = "0.1.0"
+end
